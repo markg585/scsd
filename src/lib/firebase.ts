7ba53a39f96c99fee7c120f4d071cdbac2a68dde
Mark Graham <mark@scsd.com.au>
2025-05-25 05:12:07 +1000

    Move Firebase keys to .env.local

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqbNyr8ZfeIbh5uf6xD-cj0Z8VGaGQQ30",
-  authDomain: "scsd-app-f5e79.firebaseapp.com",
-  projectId: "scsd-app-f5e79",
-  storageBucket: "scsd-app-f5e79.firebasestorage.app",
-  messagingSenderId: "71511720888",
-  appId: "1:71511720888:web:f71a1102b213d62991854d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
